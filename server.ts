cc5eb8bbfe82cf8083e8329fc18a323bebd9e481
Joseph Maramba <joseph@smartmile.eu>
2022-07-24 13:52:22 +0300

    get users api endpoint

--- server.ts
+++ server.ts
@@ -9,12 +9,50 @@ const prisma = new PrismaClient()
 
 app.use(express.json())
 
+// get all posts
 app.get("/posts", async (req, res) => {
 	const posts = await prisma.post.findMany()
+	console.log(posts)
+
 	try {
-		res.status(200).json({ posts })
+		res.status(200).json(posts)
 	} catch (error) {
 		res.status(404).send({ message: "error!" })
+	}
+})
+
+// get one post
+app.get("/posts/:id", async (req, res) => {
+	const { id } = req.params
+
+	const post = await prisma.post.findFirst({
+		where: {
+			id: Number(id),
+		},
+	})
+
+	try {
+		res.status(200).json(post)
+	} catch (error) {
+		res.status(404).json({ message: `post with id ${id} does not exist!` })
+	}
+})
+
+// get one user
+app.get("/user/:id", async (req, res) => {
+	const { id } = req.params
+
+	const user = await prisma.user.findUnique({
+		where: {
+			id: Number(id),
+		},
+		include: { posts: true },
+	})
+
+	try {
+		res.status(200).json(user)
+	} catch (error) {
+		res.status(404).json({ message: `user with id ${id} does not exist!` })
 	}
 })
 
